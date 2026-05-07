c7c9b4a1f85c5c48be7ccfd392b5c682d89292d6
Kip <kip@palewi.re>
2026-05-06 20:17:02 -0400

    Update lighthouserc.cjs

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -12,9 +12,9 @@ module.exports = {
 		},
 		assert: {
 			assertions: {
-				'categories:performance': ['error', { minScore: 0.9 }],
-				'categories:accessibility': ['error', { minScore: 0.9 }],
-				'categories:seo': ['error', { minScore: 0.9 }]
+				'categories:performance': ['error', { minScore: 0.8 }],
+				'categories:accessibility': ['error', { minScore: 0.8 }],
+				'categories:seo': ['error', { minScore: 0.8 }]
 			}
 		},
 		upload: {
